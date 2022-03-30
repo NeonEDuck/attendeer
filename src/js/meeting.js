ffc77737031dc0e9ca728fac491cc0e7f6a7ee7b
NEDuck <cj96cj9696cj@gmail.com>
2022-03-31 02:52:05 +0800

    重寫RTC連線架構
    重新架構RTC連線架構，現在不管哪一方為offerer或answerer，都可以主動丟offer來重新建立新連線。
    新架構可以讓使用者在會議時，隨意新增、刪除視訊媒體。

--- src/js/meeting.js
+++ src/js/meeting.js
@@ -12,12 +12,6 @@ const servers = {
     iceCandidatePoolSize: 10,
 };
 
-// Global variable
-const pcDict = {};
-const snapshotListeners = [];
-let localUserId = null;
-let localStream = null;
-
 // HTML elements
 const camPrefab = document.querySelector('.cam');
 const webcamBtn = document.querySelector('#webcam-btn');
@@ -30,266 +24,318 @@ const videoTray = document.querySelector('#video-tray');
 webcamBtn.disabled = true;
 callBtn.disabled = true;
 hangUpBtn.disabled = true;
+callInput.disabled = true;
 
 // Delete prefab
 camPrefab.remove()
 camPrefab.removeAttribute('hidden');
 
+// Global variable
+const peerDict = {};
+let localUserId = null;
+let localStream = null;
+let localCam = createCam();
+localCam.cam.id = `user-local`;
+videoTray.appendChild(localCam.cam);
+
+// Firestore
+const calls = collection(firestore, 'calls');
+let callDoc;
+let participants;
+let localUserDoc;
+let localClients;
+
 // Check login
 onAuthStateChanged(auth, (user) => {
-    localUserId = user.uid;
-    webcamBtn.disabled = !Boolean(localUserId);
-})
-
-function addTrackToRemoteVideo(track, userId) {
-    let remoteCam = videoTray.querySelector(`#user-${userId}`);
-    let remoteStream;
-
-    if (!remoteCam) {
-        remoteCam = camPrefab.cloneNode(true);
-        remoteCam.id = `user-${userId}`;
-        videoTray.appendChild(remoteCam);
-        remoteStream = new MediaStream();
+    if (user) {
+        localUserId = user.uid;
+        localCam.name.innerHTML = localUserId;
+        // start();
+        webcamBtn.disabled = false;
+        callBtn.disabled = false;
+        callInput.disabled = false;
     }
-    else {
-        remoteStream = remoteVideo.srcObject;
-    }
-
-    let remoteVideo = remoteCam.querySelector('.cam__video');
-    let remoteName = remoteCam.querySelector('.cam__name');
-
-    remoteName.innerHTML = userId;
-    remoteStream.addTrack(track);
-    remoteVideo.srcObject = remoteStream;
-}
-
-function createPc(userId) {
-    let pc = new RTCPeerConnection(servers);
-    localStream.getTracks().forEach((track) => {
-        pc.addTrack(track, localStream);
-    });
-
-    pc.ontrack = (event) => {
-        event.streams.forEach((stream) => {
-            stream.getTracks().forEach((track) => {
-                addTrackToRemoteVideo(track, userId);
-            });
-        });
-    };
-    return pc;
-}
+});
 
 webcamBtn.addEventListener('click', async () => {
-    localStream = await navigator.mediaDevices.getUserMedia({ video: {undefined}, audio: false });
-    let localCam = camPrefab.cloneNode(true);
-    let localVideo = localCam.querySelector('.cam__video');
-    let localName = localCam.querySelector('.cam__name');
-
-    localName.innerHTML = localUserId;
-    localVideo.srcObject = localStream;
-    videoTray.appendChild(localCam);
-
-    callBtn.disabled = false;
-    webcamBtn.disabled = true;
+    setupLocalStream();
 });
 
 callBtn.addEventListener('click', async () => {
-    const calls = collection(firestore, 'calls');
-    let callDoc;
+
     if (callInput.value) {
         callDoc = doc(calls, callInput.value);
     }
     else {
         callDoc = doc(calls);
-        await setDoc(callDoc, {});
+        await setDoc(callDoc, {})
+        callInput.value = callDoc.id;
     }
-    const participants = collection(callDoc, 'participants');
-    const localUserDoc = doc(participants, localUserId);
+    participants = collection(callDoc, 'participants');
+    localUserDoc = doc(participants, localUserId);
+    localClients = collection(localUserDoc, 'clients')
+
+    console.log('create user doc');
     await setDoc(localUserDoc, {});
 
-    callInput.value = callDoc.id;
+    setupNewUserListener();
 
-    // Offer to every other user in the call
     const userDocs = await getDocs(participants);
-    userDocs.forEach(async (remoteUserDoc) => {
-        if (remoteUserDoc.id === localUserId) {
+    userDocs.forEach(async (remoteDoc) => {
+        if (remoteDoc.id === localUserId) {
             return;
         }
 
-        const localUserDoc = doc(collection(remoteUserDoc.ref, 'clients'), localUserId);
-        const offerCandidates = collection(localUserDoc, 'offerCandidates');
-        const answerCandidates = collection(localUserDoc, 'answerCandidates');
-        pcDict[remoteUserDoc.id] = createPc(remoteUserDoc.id);
-
-        pcDict[remoteUserDoc.id].onicecandidate = (event) => {
-            event.candidate && addDoc(offerCandidates, event.candidate.toJSON());
-        };
-
-        const offerDescription = await pcDict[remoteUserDoc.id].createOffer();
-        await pcDict[remoteUserDoc.id].setLocalDescription(offerDescription);
-
-        const offer = {
-            sdp: offerDescription.sdp,
-            type: offerDescription.type,
-        };
-
-        await deleteDoc(localUserDoc);
-        await setDoc(localUserDoc, { offer });
-
-        // Listening for user to answer back
-        snapshotListeners.push(
-            onSnapshot(localUserDoc, (snapshot) => {
-                if (snapshot.exists()) {
-                    const data = snapshot.data();
-                    if (!pcDict[remoteUserDoc.id].currentRemoteDescription && data?.answer) {
-                        const answerDescription = new RTCSessionDescription(data.answer);
-                        pcDict[remoteUserDoc.id].setRemoteDescription(answerDescription);
-                    }
-                }
-                else {
-
-                    pcDict[remoteUserDoc.id].close();
-                    pcDict[remoteUserDoc.id].onicecandidate = null;
-                    pcDict[remoteUserDoc.id].onaddstream = null;
-                    pcDict[remoteUserDoc.id] = null;
-                    let remoteCam = videoTray.querySelector(`#user-${remoteUserDoc.id}`);
-                    remoteCam.remove();
-                }
-            })
-        );
-
-        // Listening for answer candidates
-        snapshotListeners.push(
-            onSnapshot(answerCandidates, (snapshot) => {
-                snapshot.docChanges().forEach((change) => {
-                    if (change.type === 'added') {
-                        let data = change.doc.data();
-                        pcDict[remoteUserDoc.id].addIceCandidate(new RTCIceCandidate(data));
-                    }
-                });
-            })
-        );
+        offerToUser(remoteDoc.id);
+        console.log('userDocs offer setupUserListener');
+        setupUserListener(remoteDoc.id);
+        setupCandidateListener(remoteDoc.id);
     });
-
-    // New clients that want us to answer
-    snapshotListeners.push(
-        onSnapshot(collection(localUserDoc, 'clients'), (snapshot) => {
-            snapshot.docChanges().forEach( async (change) => {
-                if (change.type === 'added') {
-                    const offerCandidates = collection(change.doc.ref, 'offerCandidates');
-                    const answerCandidates = collection(change.doc.ref, 'answerCandidates');
-
-                    let remoteUserDoc = change.doc.ref;
-                    pcDict[remoteUserDoc.id] = new createPc(remoteUserDoc.id);
-
-                    pcDict[remoteUserDoc.id].onicecandidate = (event) => {
-                        event.candidate && addDoc(answerCandidates, event.candidate.toJSON());
-                    };
-
-                    const docData = change.doc.data();
-                    const offerDescription = docData.offer;
-                    await pcDict[remoteUserDoc.id].setRemoteDescription(new RTCSessionDescription(offerDescription));
-
-                    const answerDescription = await pcDict[remoteUserDoc.id].createAnswer();
-                    await pcDict[remoteUserDoc.id].setLocalDescription(answerDescription);
-
-                    docData.answer = {
-                        type: answerDescription.type,
-                        sdp: answerDescription.sdp,
-                    };
-
-                    await setDoc(remoteUserDoc, docData);
-
-                    // Listening for offer candidates
-                    snapshotListeners.push(
-                        onSnapshot(offerCandidates, (snapshot) => {
-                            snapshot.docChanges().forEach((change) => {
-                                if (change.type === 'added') {
-                                    let data = change.doc.data();
-                                    pcDict[remoteUserDoc.id].addIceCandidate(new RTCIceCandidate(data));
-                                }
-                            });
-                        })
-                    );
-                }
-                else if (change.type === 'removed') {
-                    let remoteUserDoc = change.doc.ref;
-
-                    pcDict[remoteUserDoc.id].close();
-                    pcDict[remoteUserDoc.id].onicecandidate = null;
-                    pcDict[remoteUserDoc.id].onaddstream = null;
-                    pcDict[remoteUserDoc.id] = null;
-                    let remoteCam = videoTray.querySelector(`#user-${remoteUserDoc.id}`);
-                    remoteCam.remove();
-                }
-            });
-        })
-    );
 
     callBtn.disabled = true;
-    hangUpBtn.disabled = false;
 });
 
-hangUpBtn.addEventListener('click', async () => {
-    const calls = collection(firestore, 'calls');
-    const callDoc = doc(calls, callInput.value);
-    const participants = collection(callDoc, 'participants');
-    const localUserDoc = doc(participants, localUserId);
-    const userDocs = await getDocs(participants);
+async function offerToUser(remoteId) {
+    console.log('offer to user');
+    const remoteUserDoc = doc(participants, remoteId);
+    const remoteClients = collection(remoteUserDoc, 'clients');
+    const remoteClientsLocalDoc = doc(remoteClients, localUserId);
+    const offerCandidates = collection(remoteClientsLocalDoc, 'candidates');
 
-    snapshotListeners.forEach((listener) => {
-        listener();
+    addPeer(remoteId);
+    peerDict[remoteId].pc.onicecandidate = (event) => {
+        event.candidate && addDoc(offerCandidates, event.candidate.toJSON());
+    };
+
+    const offerDesc = await peerDict[remoteId].pc.createOffer({ offerToReceiveAudio: true, offerToReceiveVideo: true });
+    await peerDict[remoteId].pc.setLocalDescription(offerDesc);
+    console.log('set local offer');
+
+    // await deleteDoc(remoteClientsLocalDoc);
+    await setDoc(remoteClientsLocalDoc, {desc: JSON.stringify(peerDict[remoteId].pc.localDescription)});
+    console.log('throw offer');
+
+    // closePeer(remoteId);
+}
+
+async function answerToUser(remoteId) {
+    console.log('answer to user');
+    const remoteUserDoc = doc(participants, remoteId);
+    const remoteClients = collection(remoteUserDoc, 'clients');
+    const remoteClientsLocalDoc = doc(remoteClients, localUserId);
+
+    const answerDesc = await peerDict[remoteId].pc.createAnswer();
+    await peerDict[remoteId].pc.setLocalDescription(answerDesc);
+    console.log('set local answer');
+
+    await setDoc(remoteClientsLocalDoc, {desc: JSON.stringify(peerDict[remoteId].pc.localDescription)});
+    console.log('throw answer');
+}
+
+function setupNewUserListener() {
+    console.log('setup new user listener');
+    onSnapshot(collection(localUserDoc, 'clients'), (snapshot) => {
+        snapshot.docChanges().forEach( async (change) => {
+            if (change.type === 'added') {
+                console.log('saw new user: ', change.doc.id);
+                if (!peerDict[change.doc.id]) {
+                    addPeer(change.doc.id);
+                    console.log('setupNewUserListener setupUserListener');
+                    setupUserListener(change.doc.id);
+                    setupCandidateListener(change.doc.id);
+                }
+            }
+        });
     });
+}
 
-    userDocs.forEach(async (remoteUserDoc) => {
-        if (remoteUserDoc.id === localUserId || !Object.keys(pcDict).includes(remoteUserDoc.id)) {
+function setupUserListener(remoteId) {
+    console.log('setup user listener for: ' + remoteId);
+    const remoteDoc = doc(localClients, remoteId);
+
+    peerDict[remoteId].usersListener = onSnapshot(remoteDoc, (doc) => {
+        listenerLogic(doc);
+    });
+}
+
+function setupCandidateListener(remoteId) {
+    console.log('setup candidate listener');
+    const remoteDoc = doc(localClients, remoteId);
+
+    if (peerDict[remoteId].candidatesListener) {
+        peerDict[remoteId].candidatesListener();
+    }
+
+    peerDict[remoteId].candidatesListener = onSnapshot(collection(remoteDoc, 'candidates'), (snapshot) => {
+        snapshot.docChanges().forEach( async (change) => {
+            if (change.type === 'added') {
+                console.log('saw new candidate');
+                let data = change.doc.data();
+                peerDict[remoteId].pc.addIceCandidate(new RTCIceCandidate(data));
+            }
+        });
+    });
+}
+
+function listenerLogic(doc) {
+    let { desc } = doc.data() || {};
+    if ( desc ) {
+        desc = JSON.parse(desc);
+        if (desc.type === 'answer') {
+            console.log('saw answer');
+            peerDict[doc.id].pc.setRemoteDescription(desc);
+            console.log('set remote answer');
+        }
+        else if (desc.type === 'offer') {
+            console.log('saw offer');
+            addPeer(doc.id);
+            peerDict[doc.id].pc.setRemoteDescription(desc);
+            console.log('set remote offer');
+            // console.log(peerDict[doc.id].usersListener)
+            // console.log(!peerDict[doc.id].usersListener)
+            // if (!peerDict[doc.id].usersListener) {
+            //     setupUserListener(doc.id);
+            // }
+            answerToUser(doc.id);
+        }
+        else {
+            console.error('unknown sdp type');
             return;
         }
+        createRemoteCam(doc.id, 0);
+    }
+}
 
-        pcDict[remoteUserDoc.id].close();
-        pcDict[remoteUserDoc.id].onicecandidate = null;
-        pcDict[remoteUserDoc.id].ontrack = null;
-        pcDict[remoteUserDoc.id] = null;
-        let remoteCam = videoTray.querySelector(`#user-${remoteUserDoc.id}`);
-        remoteCam.remove();
+function closePeer(id) {
+    console.log('close peer');
+    if (peerDict[id]) {
+        if (peerDict[id].usersListener) {
+            peerDict[id].usersListener();
+        }
+        if (peerDict[id].candidatesListener) {
+            peerDict[id].candidatesListener();
+        }
+        if (peerDict[id].pc) {
+            peerDict[id].pc.close();
+            peerDict[id].pc.onicecandidate = null;
+            peerDict[id].pc.onaddstream = null;
+            peerDict[id].pc = null;
+        }
+    }
+}
 
-        console.dir(doc(collection(remoteUserDoc.ref, 'clients'), localUserId));
-        const localUserDoc = doc(collection(remoteUserDoc.ref, 'clients'), localUserId);
-        await deleteDoc(localUserDoc);
+function addPeer(id) {
+    if (!peerDict[id]) {
+        console.log('add peer');
+        let [pc, senders] = createPC(id);
+        peerDict[id] = {
+            pc,
+            senders: {
+                webcam: senders
+            },
+            usersListener: null,
+            candidatesListener: null,
+        };
+    }
+}
+
+function createPC(userId) {
+    console.log('create PC');
+    let pc = new RTCPeerConnection(servers);
+    let senders = [];
+    // pc.onaddstream = () => {
+    //     console.log('onaddstream');
+    // }
+    localStream?.getTracks().forEach((track) => {
+        console.log('set track');
+        senders.push(pc.addTrack(track, localStream));
     });
 
-    let clientDocs = await getDocs(collection(localUserDoc, 'clients'));
-    clientDocs.forEach(async (clientDoc) => {
-        let offerCandidates = await getDocs(collection(clientDoc.ref, 'offerCandidates'));
-        let answerCandidates = await getDocs(collection(clientDoc.ref, 'answerCandidates'));
-        offerCandidates.forEach(async (offerDoc) => {
-            await deleteDoc(offerDoc.ref);
-        })
-        answerCandidates.forEach(async (answerDoc) => {
-            await deleteDoc(answerDoc.ref);
-        })
-        await deleteDoc(clientDoc.ref);
-    });
-    await deleteDoc(localUserDoc);
+    console.log('set ontrack');
+    pc.ontrack = (event) => {
+        console.log('ontrack');
+        event.streams.forEach((stream) => {
+            stream.getTracks().forEach((track, idx) => {
+                addTrackToRemoteVideo(track, userId, idx);
+            });
+        });
+    };
+    // pc.onconnectionstatechange = (event) => {
+    //     switch (pc.connectionState) {
+    //         case "connected":
+    //             console.log('set onnegotiationneeded');
+    //             pc.onnegotiationneeded = () => {
+    //                 console.log('onnegotiationneeded');
+    //                 for (const id in peerDict) {
+    //                     offerToUser(id);
+    //                 }
+    //             };
+    //             break;
+    //     }
+    // };
 
-    callBtn.disabled = false;
-    hangUpBtn.disabled = true;
-});
+    return [pc, senders];
+}
 
-// window.addEventListener('beforeOnLoad', (e) => {
-//     e.preventDefault();
+function createRemoteCam(userId, idx) {
+    console.log(`add remote cam for ${userId}`);
+    let remoteCam = videoTray.querySelector(`#user-${userId}-${idx}`);
 
-//     if (confirm('You sure you want to leave?')) {
-//         alert('good bye');
-//     }
-//     // else {
+    if (!remoteCam) {
+        let { cam, name } = createCam();
+        cam.id = `user-${userId}-${idx}`;
+        name.innerHTML = userId;
 
-//     // }
+        remoteCam = cam;
+        videoTray.appendChild(remoteCam);
+    }
+    return remoteCam;
+}
 
-//     // e.returnValue = 'Do you want to leave?';
-//     return '';
-// });
+function addTrackToRemoteVideo(track, userId, idx) {
+    console.log('addTrackToRemoteVideo');
+    let remoteCam = createRemoteCam(userId, idx);
+    let remoteVideo = remoteCam.querySelector('.cam__video');
+    let remoteStream = new MediaStream();
+    remoteStream.addTrack(track);
+    remoteVideo.srcObject = remoteStream;
+}
 
-// TODO: 斷線問題處理，如果回來時使用者底下有answer或offer的話會導致不預期的狀況發生
-// TODO: - 主因在於斷線時，使用者資料不會被刪除，造成房間有新成員加入時，會照樣傳送offer給不再通話(斷線)的使用者
-// TODO: - 所以當使用者連線回來時，會導致問題發生。
+let webcamOn = false;
+async function setupLocalStream() {
+    if (!localStream) {
+        console.log('init webcam');
+        localStream = await navigator.mediaDevices.getUserMedia({ video: {undefined}, audio: false });
+        localCam.video.srcObject = localStream;
+    }
+
+    if (webcamOn) {
+        console.log('turn off webcam');
+        for (const [id, peer] of Object.entries(peerDict)) {
+            for (let sender of peer.senders.webcam) {
+                console.log(`remove from ${id} ${sender}`)
+                peer.pc.removeTrack(sender);
+            }
+            peer.senders.webcam = [];
+            offerToUser(id);
+        }
+    }
+    else {
+        console.log('turn on webcam');
+        for (const [id, peer] of Object.entries(peerDict)) {
+            localStream.getTracks().forEach((track) => {
+                console.log(`add to ${id}`)
+                peer.senders.webcam.push(peer.pc.addTrack(track, localStream));
+            });
+            offerToUser(id);
+        }
+    }
+    webcamOn = !webcamOn;
+}
+
+function createCam() {
+    let cam = camPrefab.cloneNode(true);
+    let video = cam.querySelector('.cam__video');
+    let name = cam.querySelector('.cam__name');
+    return { cam, video, name };
+}
