e3b263c9287eb8b149cf09a563c37eff32e19057
NEDuck <cj96cj9696cj@gmail.com>
2022-07-29 13:37:06 +0800

    新增class-detail頁面樣式

--- src/js/classroom.js
+++ src/js/classroom.js
@@ -1,9 +1,11 @@
 import { onSnapshot, collection, doc, getDoc, setDoc, updateDoc, deleteDoc, getDocs, query, where, limit } from "firebase/firestore";
 import { firestore } from "./firebase-config.js";
+import './base.js'
 import { getUser } from './util.js';
 
 const className = document.querySelector('#class-name');
 const callBtn = document.querySelector('#call-btn');
+const classSchedule = document.querySelector('#class-schedule');
 const callId = document.querySelector('#call-id')?.value?.trim() || document.querySelector('#call-id').innerHTML?.trim();
 
 const calls = collection(firestore, 'calls');
@@ -18,6 +20,11 @@ document.onreadystatechange = async () => {
 
     className.innerHTML = name;
 
+    const rows = classSchedule.querySelector('tbody').querySelectorAll('tr');
+    for (const row of rows) {
+        const cells = row.querySelectorAll('td');
+        cells[3].classList.add('on');
+    }
 };
 
 callBtn.addEventListener('click', () => {
