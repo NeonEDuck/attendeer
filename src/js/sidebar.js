96cf9763545cf39ff8569fe8e613d112c5288e77
uliang <amisleo000@gmail.com>
2022-11-01 10:19:54 +0800

    輸出輸入裝置設定(初步)

--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -32,6 +32,7 @@ const voteSetting                = document.querySelector('.vote-setting');
 const fltCntr             = document.querySelector(".floating-container"),
       closeFloatingButton = fltCntr.querySelector('.close-floating_button'),
       floatingAlert       = fltCntr.querySelector('#floating-alert'),
+      floatingUserMedia       = fltCntr.querySelector('#floating-user-media'),
       alertInfo           = fltCntr.querySelector('.alert-info'),
       infoType            = alertInfo.querySelector('#info-type'),
       infoInterval        = floatingAlert.querySelector("#info-interval"),
@@ -104,23 +105,26 @@ export function sidebarListener() {
         item.addEventListener("click",async (e) => {
             Array.from(navBtn).forEach((item) => {
                 item.className = "nav-btn";
-                floatingAlert.style.opacity = 0;
             });
             navBtn[index].classList.toggle("open");
             let navText = navBtn[index].querySelector('.nav-text').innerHTML;
             fltCntr.hidden = false;
-
+            hiddenAllFloating();
+            
             if(navText === '警醒資訊') {
                 const { host } = (await getDoc(callDoc)).data();
                 const user = await getUser();
                 localUserId = user.uid;
-
+                floatingAlert.hidden = false;
                 if (localUserId === host){
                     console.log('會議主辦人警醒資訊');
                     closeModalForm();
                 }else {
                     console.log('不是主辦人只提供個人警醒資訊');
                 }
+            }else if( navText === '裝置設定' ) {
+                floatingUserMedia.hidden = false;
+                userMedia();
             }
         });
     });
@@ -134,23 +138,26 @@ closeFloatingButton.addEventListener('click', () => {
         item.className = "nav-btn";
     });
 
+    hiddenAllFloating();
+});
+
+function hiddenAllFloating() {
+    optionsTotal = 0;
+    current = 0;
+    floatingUserMedia.hidden = true;
+    floatingAlert.hidden = true;
     alertInfo.classList.remove('close');
     alertChoose.classList.add("close");
     buttonSetting.hidden = true;
     multipleChoiceSetting.hidden = true;
     essayQuestion.hidden = true;
     voteSetting.hidden = true;
-
-    optionsTotal = 0;
-    current = 0;
-
     const alertStepProgress = document.querySelectorAll('.alert-step-progress');
     alertStepProgress.forEach( alertStepProgress => {
         alertStepProgress.innerHTML = '';
     })
-
     alertInfo.classList.remove('Revise');
-});
+}
 
 //警醒浮動視窗 開啟
 async function closeModalForm() {
@@ -998,4 +1005,128 @@ choose4.addEventListener('click', () => {
 
 async function AlertReplace() {
     setupAlertScheduler();
+}
+
+function userMedia() {
+    // 取得標籤
+    const videoElement = document.querySelector('video')
+    const audioInputSelect = document.querySelector('select#audioSource')
+    const audioOutputSelect = document.querySelector('select#audioOutput')
+    const videoSelect = document.querySelector('select#videoSource')
+    const selectors = [audioInputSelect, audioOutputSelect, videoSelect]
+
+    audioOutputSelect.disabled = !('sinkId' in HTMLMediaElement.prototype)
+
+    // 將讀取到的設備加入到 select 標籤中
+    function gotDevices(deviceInfos) {
+        // Handles being called several times to update labels. Preserve values.
+        const values = selectors.map((select) => select.value)
+        selectors.forEach((select) => {
+            while (select.firstChild) {
+            select.removeChild(select.firstChild)
+            }
+        })
+        for (let i = 0; i !== deviceInfos.length; ++i) {
+            const deviceInfo = deviceInfos[i]
+            const option = document.createElement('option')
+            option.value = deviceInfo.deviceId
+            if (deviceInfo.kind === 'audioinput') {
+                option.text =
+                deviceInfo.label || `microphone ${audioInputSelect.length + 1}`
+                audioInputSelect.appendChild(option)
+            } else if (deviceInfo.kind === 'audiooutput') {
+            option.text =
+                deviceInfo.label || `speaker ${audioOutputSelect.length + 1}`
+                audioOutputSelect.appendChild(option)
+            } else if (deviceInfo.kind === 'videoinput') {
+                option.text = deviceInfo.label || `camera ${videoSelect.length + 1}`
+                videoSelect.appendChild(option)
+            } else {
+                console.log('Some other kind of source/device: ', deviceInfo)
+            }
+        }
+        selectors.forEach((select, selectorIndex) => {
+            if (
+                Array.prototype.slice
+                    .call(select.childNodes)
+                    .some((n) => n.value === values[selectorIndex])
+            ) {
+                select.value = values[selectorIndex]
+            }
+        })
+    }
+
+    // 讀取設備
+    navigator.mediaDevices.enumerateDevices().then(gotDevices).catch(handleError)
+
+    // 手動修改音訊的輸出 例如預設耳機切換成喇叭
+    function attachSinkId(element, sinkId) {
+        if (typeof element.sinkId !== 'undefined') {
+            element
+            .setSinkId(sinkId)
+            .then(() => {
+                console.log(`Success, audio output device attached: ${sinkId}`)
+            })
+            .catch((error) => {
+                let errorMessage = error
+                if (error.name === 'SecurityError') {
+                    errorMessage = `You need to use HTTPS for selecting audio output device: ${error}`
+                }
+                console.error(errorMessage)
+                // Jump back to first output device in the list as it's the default.
+                audioOutputSelect.selectedIndex = 0
+            })
+        } else {
+            console.warn('Browser does not support output device selection.')
+        }
+    }
+
+    // 處理音訊改變的方法
+    function changeAudioDestination() {
+        const audioDestination = audioOutputSelect.value
+        attachSinkId(videoElement, audioDestination)
+    }
+
+    // 將視訊顯示在 video 標籤上
+    function gotStream(stream) {
+        videoElement.srcObject = stream
+
+        return navigator.mediaDevices.enumerateDevices()
+    }
+
+    // 錯誤處理
+    function handleError(error) {
+        console.log(
+            'navigator.MediaDevices.getUserMedia error: ',
+            error.message,
+            error.name,
+        )
+    }
+
+    // 播放自己的視訊
+    function start() {
+        if (window.stream) {
+            window.stream.getTracks().forEach((track) => {
+                track.stop()
+            })
+        }
+        const audioSource = audioInputSelect.value
+        const videoSource = videoSelect.value
+        const constraints = {
+            audio: { deviceId: audioSource ? { exact: audioSource } : undefined },
+            video: { deviceId: videoSource ? { exact: videoSource } : undefined },
+        }
+        navigator.mediaDevices
+            .getUserMedia(constraints)
+            .then(gotStream)
+            .then(gotDevices)
+            .catch(handleError)
+    }
+
+    audioInputSelect.onchange = start
+    audioOutputSelect.onchange = changeAudioDestination
+
+    videoSelect.onchange = start
+
+    start();
 }
